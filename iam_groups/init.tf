94a8f813a8e0457a0c0dcb64c5672ca94e7d6a0e
Chima Onukwuru <40035609+ghouldaemon@users.noreply.github.com>
2025-02-07 13:35:04 -0500

    Update init.tf

--- iam_groups/init.tf
+++ iam_groups/init.tf
@@ -3,7 +3,7 @@ terraform {
     encrypt = true
     bucket = "state-terraform"
     key    = "iam_groups/terraform.tfstate"
-    region = "eu-west-1"
+    region = "us-east-1"
     dynamodb_table = "state-terraform"
   }
 }
